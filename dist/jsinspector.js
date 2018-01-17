d254a3c536a55212e5fc59a315ec15cf04eb2aff
Ryan Chen <ryan.chen@platfora.com>
2018-01-16 22:01:18 -0800

    Update jsinspector

--- dist/jsinspector.js
+++ dist/jsinspector.js
@@ -60,19 +60,49 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports) {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Base =
+/*#__PURE__*/
+function () {
+  function Base() {
+    _classCallCheck(this, Base);
+  }
+
+  _createClass(Base, [{
+    key: "fire",
+    value: function fire(event) {}
+  }, {
+    key: "on",
+    value: function on() {}
+  }]);
+
+  return Base;
+}();
+
+module.exports = Base;
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(1);
+__webpack_require__(2);
 
-var _App = _interopRequireDefault(__webpack_require__(8));
+var _App = _interopRequireDefault(__webpack_require__(3));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -82,25 +112,23 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 new _App.default();
 
 /***/ }),
-/* 1 */
+/* 2 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ }),
-/* 2 */,
-/* 3 */,
-/* 4 */,
-/* 5 */,
-/* 6 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _Base2 = _interopRequireDefault(__webpack_require__(7));
+var _Base2 = _interopRequireDefault(__webpack_require__(0));
 
-var _ToolBox = _interopRequireDefault(__webpack_require__(9));
+var _Editor = _interopRequireDefault(__webpack_require__(4));
+
+var _Console = _interopRequireDefault(__webpack_require__(7));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -117,6 +145,118 @@ function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) ===
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+var App =
+/*#__PURE__*/
+function (_Base) {
+  _inherits(App, _Base);
+
+  function App() {
+    var _this;
+
+    _classCallCheck(this, App);
+
+    _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this)); // this.editor = this.createEditor();
+
+    _this.editor = new _Editor.default({
+      container: document.getElementById('editor'),
+      app: _assertThisInitialized(_this)
+    });
+    _this.console = new _Console.default({
+      editor: _this.editor
+    });
+    _this.inspectBtn = document.getElementById('inspectBtn'); // Config inspect behavior. 
+    // This will trigger browser inspector.
+    // let inspectBtn = document.getElementById('inspectBtn');
+
+    _this.listen();
+
+    return _this;
+  }
+
+  _createClass(App, [{
+    key: "listen",
+    value: function listen() {
+      var _this2 = this;
+
+      this.inspectBtn.addEventListener('click', function () {
+        var editor = _this2.editor.aceEditor,
+            lineContents = editor.getValue().split('\n'),
+            result = '',
+            evalContent; // TODO: Avoid nested eval
+        // TODO: Undo and Redo
+
+        _this2.console.startNewLog(); // Add breakpoints
+
+
+        editor.session.getBreakpoints().forEach(function (breakPointClass, row) {
+          if (breakPointClass.indexOf('disabled') === -1) {
+            lineContents[row] = 'debugger; /* Line ' + (row + 1) + ' */' + lineContents[row];
+          }
+        });
+        evalContent = '// Start debug your javascript...\ndebugger;\n\n' + lineContents.join('\n'); // hijack console.log
+
+        var oldConsoleLog = console.log;
+
+        console.log = function () {
+          for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+            args[_key] = arguments[_key];
+          }
+
+          _this2.console.addLog('> ' + args);
+
+          oldConsoleLog(args);
+        }; // It's ok to use eval as this will be a static page
+
+
+        try {
+          eval(evalContent);
+        } catch (e) {
+          _this2.console.resultElement.innerHTML = '<pre class="result error">Your javascript code has error(s)</pre>';
+        } // Restore console.log
+
+
+        console.log = oldConsoleLog; // if (this.console.resultElement.innerHTML === '') {
+        //   this.console.resultElement.innerHTML = '<pre class="result">' + result + '</pre>'
+        // }
+      });
+    }
+  }]);
+
+  return App;
+}(_Base2.default);
+
+;
+module.exports = App;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _Base2 = _interopRequireDefault(__webpack_require__(0));
+
+var _ToolBox = _interopRequireDefault(__webpack_require__(5));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+var defaultCode = 'function sum (a, b) {\n  return a + b;\n}\n\nsum(1,2);';
 
 var Editor =
 /*#__PURE__*/
@@ -175,21 +315,35 @@ function (_Base) {
       this.aceEditor.on('gutterclick', function (e) {
         var row = e.getDocumentPosition().row;
         _this2.toggleBreakPoint(row) ? _this2.toolBox.addToBreakpointLines(row) : _this2.toolBox.removeFromBreakpointLines(row);
+        localStorage.setItem('jsinspectorBreakpoints', JSON.stringify(_this2.aceEditor.session.getBreakpoints()));
       }); // Store changes into localStorage
 
       this.aceEditor.session.on('change', function () {
-        _this2.app.console.resultElement.innerHTML = '';
         localStorage.setItem('jsinspectorContent', _this2.aceEditor.getValue());
       });
     }
   }, {
     key: "restoreCode",
     value: function restoreCode() {
+      var _this3 = this;
+
       // Retrieve Code from localStorage, if any
-      var content = localStorage.getItem('jsinspectorContent');
+      var content = localStorage.getItem('jsinspectorContent'),
+          breakpoints = JSON.parse(localStorage.getItem('jsinspectorBreakpoints') || '[]');
 
       if (content) {
         this.aceEditor.setValue(content);
+        breakpoints.forEach(function (breakpointClass, row) {
+          if (breakpointClass) {
+            _this3.toggleBreakPoint(row);
+
+            _this3.toolBox.addToBreakpointLines(row);
+          }
+        });
+      } else {
+        this.aceEditor.setValue(defaultCode);
+        this.toggleBreakPoint(1);
+        this.toolBox.addToBreakpointLines(1);
       }
     }
   }]);
@@ -200,141 +354,15 @@ function (_Base) {
 module.exports = Editor;
 
 /***/ }),
-/* 7 */
-/***/ (function(module, exports) {
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Base =
-/*#__PURE__*/
-function () {
-  function Base() {
-    _classCallCheck(this, Base);
-  }
-
-  _createClass(Base, [{
-    key: "fire",
-    value: function fire(event) {}
-  }, {
-    key: "on",
-    value: function on() {}
-  }]);
-
-  return Base;
-}();
-
-module.exports = Base;
-
-/***/ }),
-/* 8 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _Base2 = _interopRequireDefault(__webpack_require__(7));
+var _Base2 = _interopRequireDefault(__webpack_require__(0));
 
-var _Editor = _interopRequireDefault(__webpack_require__(6));
-
-var _Console = _interopRequireDefault(__webpack_require__(10));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-var App =
-/*#__PURE__*/
-function (_Base) {
-  _inherits(App, _Base);
-
-  function App() {
-    var _this;
-
-    _classCallCheck(this, App);
-
-    _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this)); // this.editor = this.createEditor();
-
-    _this.editor = new _Editor.default({
-      container: document.getElementById('editor'),
-      app: _assertThisInitialized(_this)
-    });
-    _this.console = new _Console.default({
-      editor: _this.editor
-    });
-    _this.inspectBtn = document.getElementById('inspectBtn'); // Config inspect behavior. 
-    // This will trigger browser inspector.
-    // var inspectBtn = document.getElementById('inspectBtn');
-
-    _this.listen();
-
-    return _this;
-  }
-
-  _createClass(App, [{
-    key: "listen",
-    value: function listen() {
-      var _this2 = this;
-
-      this.inspectBtn.addEventListener('click', function () {
-        var editor = _this2.editor.aceEditor,
-            lineContents = editor.getValue().split('\n'),
-            evalContent,
-            result; // TODO: Avoid nested eval
-        // TODO: Undo and Redo
-        // Add breakpoints
-
-        editor.session.getBreakpoints().forEach(function (breakPointClass, row) {
-          if (breakPointClass.indexOf('disabled') === -1) {
-            lineContents[row] = 'debugger; /* Line ' + (row + 1) + ' */' + lineContents[row];
-          }
-        });
-        evalContent = '// Start debug your javascript...\ndebugger;\n\n' + lineContents.join('\n'); // It's ok to use eval as this will be a static page
-
-        try {
-          result = eval(evalContent);
-        } catch (e) {
-          _this2.console.resultElement.innerHTML = '<span class="result error">Your javascript code has error(s)</span>';
-        }
-
-        if (_this2.console.resultElement.innerHTML === '') {
-          _this2.console.resultElement.innerHTML = '<span class="result">Result: ' + result + '</span>';
-        }
-      });
-    }
-  }]);
-
-  return App;
-}(_Base2.default);
-
-;
-module.exports = App;
-
-/***/ }),
-/* 9 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _Base2 = _interopRequireDefault(__webpack_require__(7));
-
-var _util = _interopRequireDefault(__webpack_require__(11));
+var _util = _interopRequireDefault(__webpack_require__(6));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -453,19 +481,42 @@ function (_Base) {
 module.exports = ToolBox;
 
 /***/ }),
-/* 10 */
+/* 6 */
+/***/ (function(module, exports) {
+
+var util = {
+  closestElement: function closestElement(startElement, className) {
+    while (startElement) {
+      if (startElement.classList.contains(className)) {
+        return startElement;
+      }
+
+      startElement = startElement.parentElement;
+    }
+
+    return null;
+  }
+};
+module.exports = util;
+
+/***/ }),
+/* 7 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _Base2 = _interopRequireDefault(__webpack_require__(7));
+var _Base2 = _interopRequireDefault(__webpack_require__(0));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
@@ -486,29 +537,41 @@ function (_Base) {
     return _this;
   }
 
+  _createClass(Console, [{
+    key: "addLog",
+    value: function addLog(log) {
+      this.resultElement.innerHTML += '<pre class="result">' + log + '</pre>';
+    }
+  }, {
+    key: "clearLog",
+    value: function clearLog() {
+      this.resultElement.innerHTML = '';
+    }
+  }, {
+    key: "startNewLog",
+    value: function startNewLog() {
+      var oldLogs = this.resultElement.querySelectorAll('pre.result'),
+          needSplitter = false;
+      oldLogs.forEach(function (node) {
+        if (!node.classList.contains('old')) {
+          node.classList.add('old');
+          needSplitter = true;
+        }
+      });
+
+      if (needSplitter) {
+        this.resultElement.querySelectorAll('hr').forEach(function (node) {
+          node.classList.add('old');
+        });
+        this.resultElement.innerHTML += '<hr />';
+      }
+    }
+  }]);
+
   return Console;
 }(_Base2.default);
 
 module.exports = Console;
-
-/***/ }),
-/* 11 */
-/***/ (function(module, exports) {
-
-var util = {
-  closestElement: function closestElement(startElement, className) {
-    while (startElement) {
-      if (startElement.classList.contains(className)) {
-        return startElement;
-      }
-
-      startElement = startElement.parentElement;
-    }
-
-    return null;
-  }
-};
-module.exports = util;
 
 /***/ })
 /******/ ]);
